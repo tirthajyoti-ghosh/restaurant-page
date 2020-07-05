fe301bfe5ed64a6b0ddfc23208db28c81805c2f4
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-06 00:17:16 +0530

    Add dynamic header creation (incomplete)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+(() => {
+  const content = document.getElementById('content');
+  const header = document.createElement('header');
+  const nav = document.createElement('nav');
+  const link = document.createElement('a');
+  link.setAttribute('href', '#');
+  link.setAttribute('id', 'logo');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', 'https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d38248bc65d40b464206a96_logo-white.png');
+  img.setAttribute('width', '70');
+
+  
+})();
