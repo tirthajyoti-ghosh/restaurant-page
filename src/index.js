960adf99e938196a56b867072bf6cd12fb36e737
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-06 14:31:52 +0530

    Add dynamic home

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 
-(() => {
-  const content = document.getElementById('content');
+const content = document.getElementById('content');
+
+const header = (() => {
   const header = document.createElement('header');
   const nav = document.createElement('nav');
   const link = document.createElement('a');
@@ -54,5 +55,45 @@ import './style.css';
 
   header.appendChild(nav);
 
-  content.appendChild(header);
+  return header;
 })();
+
+content.appendChild(header);
+
+const home = (() => {
+  const homeSection = document.createElement('section');
+  homeSection.setAttribute('class', 'home');
+
+  const leftDiv = document.createElement('div');
+  leftDiv.setAttribute('class', 'left');
+
+  const homeText = document.createElement('div');
+  homeText.setAttribute('class', 'home__text');
+
+  const homeHeading = document.createElement('h1');
+  homeHeading.setAttribute('class', 'home__heading');
+  homeHeading.innerText = 'Hot and fresh mussels in pots.';
+  homeText.appendChild(homeHeading);
+  
+  const ctaBtn = document.createElement('a');
+  ctaBtn.setAttribute('href', '#');
+  ctaBtn.setAttribute('class', 'cta-btn');
+  ctaBtn.innerText = 'Products';
+  homeText.appendChild(ctaBtn);
+
+  leftDiv.appendChild(homeText);
+
+  const rightDiv = document.createElement('div');
+  rightDiv.setAttribute('class', 'right');
+  const img = document.createElement('img');
+  img.setAttribute('src', 'https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d3da99cb1eba4bb6d1caf2a_welcome.jpg');
+
+  rightDiv.appendChild(img);
+
+  homeSection.appendChild(leftDiv);
+  homeSection.appendChild(rightDiv);
+
+  return homeSection;
+})();
+
+content.appendChild(home);
