eef10cc4b8962453bf13acde887b82f44378e930
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-06 22:21:20 +0530

    Add dynamic branches section

--- src/branches.js
+++ src/branches.js
@@ -1,8 +1,47 @@
 const branches = (() => {
-  let h1 = document.createElement('h1');
-  h1.innerText = 'Branches!';
+  const branchesSection = document.createElement('section');
+  branchesSection.setAttribute('class', 'branches');
 
-  return h1;
+  const branchesHeading = document.createElement('h2');
+  branchesHeading.setAttribute('class', 'branches__heading');
+  branchesHeading.innerText = 'mussels';
+
+  branchesSection.appendChild(branchesHeading);
+  
+  const branchList = document.createElement('div');
+  branchList.setAttribute('class', 'branch__list');
+  
+  const branch = (src, heading, text) => {
+    const branchDiv = document.createElement('div');
+    branchDiv.setAttribute('class', 'branch');
+    
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    
+    branchDiv.appendChild(img);
+    
+    const branchHeading = document.createElement('h3');
+    branchHeading.setAttribute('class', 'branch__heading');
+    branchHeading.innerText = heading;
+    
+    branchDiv.appendChild(branchHeading);
+    
+    const branchText = document.createElement('div');
+    branchText.setAttribute('class', 'branch__text');
+    branchText.innerHTML = text;
+    
+    branchDiv.appendChild(branchText);
+    
+    return branchDiv;
+  };
+  
+  branchList.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d44ab0328d7ff37d2c7f1ca_bahcelievler.jpg', 'Cookware Midye Express Bahçelievler', '<strong>Address:</strong>  Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email:</strong> info@tenceremidye.com<br><strong>Instagram:</strong> instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+
+  branchList.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5e3fe89c6615a6644c5d7c8a_DSCF3033.jpg', 'Saucepan Mussel Plus Beysukent', '<strong>Address:</strong> Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email</strong>: info@tenceremidye.com<br><strong>Instagram</strong>: instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+
+  branchesSection.appendChild(branchList);
+
+  return branchesSection;
 })();
 
 export default branches;
