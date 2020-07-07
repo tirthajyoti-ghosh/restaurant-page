5fd108cd132d5fecf16df9799e6ff023ba938161
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-07 20:56:42 +0530

    Refactor branches section to use modular code

--- src/js/branches.js
+++ src/js/branches.js
@@ -1,47 +1,58 @@
 const branches = (() => {
-  const branchesSection = document.createElement('section');
-  branchesSection.setAttribute('class', 'branches');
+  const branchesHeading = () => {
+    const branchesHeadingTag = document.createElement('h2');
+    branchesHeadingTag.setAttribute('class', 'branches__heading');
+    branchesHeadingTag.innerText = 'Visit Us';
 
-  const branchesHeading = document.createElement('h2');
-  branchesHeading.setAttribute('class', 'branches__heading');
-  branchesHeading.innerText = 'Visit Us';
-
-  branchesSection.appendChild(branchesHeading);
-
-  const branchList = document.createElement('div');
-  branchList.setAttribute('class', 'branch__list');
-
-  const branch = (src, heading, text) => {
-    const branchDiv = document.createElement('div');
-    branchDiv.setAttribute('class', 'branch');
-
-    const img = document.createElement('img');
-    img.setAttribute('src', src);
-
-    branchDiv.appendChild(img);
-
-    const branchHeading = document.createElement('h3');
-    branchHeading.setAttribute('class', 'branch__heading');
-    branchHeading.innerText = heading;
-
-    branchDiv.appendChild(branchHeading);
-
-    const branchText = document.createElement('div');
-    branchText.setAttribute('class', 'branch__text');
-    branchText.innerHTML = text;
-
-    branchDiv.appendChild(branchText);
-
-    return branchDiv;
+    return branchesHeadingTag;
   };
 
-  branchList.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d44ab0328d7ff37d2c7f1ca_bahcelievler.jpg', 'Cookware Midye Express Bahçelievler', '<strong>Address:</strong>  Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email:</strong> info@tenceremidye.com<br><strong>Instagram:</strong> instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+  const branchList = () => {
+    const branch = (src, heading, text) => {
+      const branchDiv = document.createElement('div');
+      branchDiv.setAttribute('class', 'branch');
 
-  branchList.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5e3fe89c6615a6644c5d7c8a_DSCF3033.jpg', 'Saucepan Mussel Plus Beysukent', '<strong>Address:</strong> Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email</strong>: info@tenceremidye.com<br><strong>Instagram</strong>: instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+      const img = document.createElement('img');
+      img.setAttribute('src', src);
 
-  branchesSection.appendChild(branchList);
+      branchDiv.appendChild(img);
 
-  return branchesSection;
+      const branchHeading = document.createElement('h3');
+      branchHeading.setAttribute('class', 'branch__heading');
+      branchHeading.innerText = heading;
+
+      branchDiv.appendChild(branchHeading);
+
+      const branchText = document.createElement('div');
+      branchText.setAttribute('class', 'branch__text');
+      branchText.innerHTML = text;
+
+      branchDiv.appendChild(branchText);
+
+      return branchDiv;
+    };
+
+    const branchListDiv = document.createElement('div');
+    branchListDiv.setAttribute('class', 'branch__list');
+
+    branchListDiv.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d44ab0328d7ff37d2c7f1ca_bahcelievler.jpg', 'Cookware Midye Express Bahçelievler', '<strong>Address:</strong>  Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email:</strong> info@tenceremidye.com<br><strong>Instagram:</strong> instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+
+    branchListDiv.appendChild(branch('https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5e3fe89c6615a6644c5d7c8a_DSCF3033.jpg', 'Saucepan Mussel Plus Beysukent', '<strong>Address:</strong> Bahçelievler mahalle 54th street (6th street)<br>31-33 / A (10)<br>Çankaya / Ankara<br>06490<br><strong>Email</strong>: info@tenceremidye.com<br><strong>Instagram</strong>: instagram.com/tenceremidye<br><strong>Phone:</strong> 0312 212 3736'));
+
+    return branchListDiv;
+  };
+
+  const branchesSection = () => {
+    const branchesSectionTag = document.createElement('section');
+    branchesSectionTag.setAttribute('class', 'branches');
+
+    branchesSectionTag.appendChild(branchesHeading());
+    branchesSectionTag.appendChild(branchList());
+
+    return branchesSectionTag;
+  };
+
+  return branchesSection();
 })();
 
 export default branches;
