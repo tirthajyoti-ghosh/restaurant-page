13441e572f22a8913508507d8405b0e2ac3041b8
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-07 19:21:21 +0530

    Refactor home.js to use modular code

--- src/js/home.js
+++ src/js/home.js
@@ -1,37 +1,56 @@
 const home = (() => {
-  const homeSection = document.createElement('section');
-  homeSection.setAttribute('class', 'home');
+  const leftDiv = () => {
+    const leftDivTag = document.createElement('div');
+    leftDivTag.setAttribute('class', 'left');
 
-  const leftDiv = document.createElement('div');
-  leftDiv.setAttribute('class', 'left');
+    const homeText = () => {
+      const homeTextDiv = document.createElement('div');
+      homeTextDiv.setAttribute('class', 'home__text');
 
-  const homeText = document.createElement('div');
-  homeText.setAttribute('class', 'home__text');
+      const homeHeading = document.createElement('h1');
+      homeHeading.setAttribute('class', 'home__heading');
+      homeHeading.innerText = 'Hot and fresh mussels in pots 😋!';
 
-  const homeHeading = document.createElement('h1');
-  homeHeading.setAttribute('class', 'home__heading');
-  homeHeading.innerText = 'Hot and fresh mussels in pots 😋!';
-  homeText.appendChild(homeHeading);
+      const ctaBtn = document.createElement('a');
+      ctaBtn.setAttribute('href', '#');
+      ctaBtn.setAttribute('class', 'cta-btn');
+      ctaBtn.innerText = 'What are you waiting for? ';
 
-  const ctaBtn = document.createElement('a');
-  ctaBtn.setAttribute('href', '#');
-  ctaBtn.setAttribute('class', 'cta-btn');
-  ctaBtn.innerText = 'What are you waiting for? ';
-  homeText.appendChild(ctaBtn);
 
-  leftDiv.appendChild(homeText);
+      homeTextDiv.appendChild(homeHeading);
+      homeTextDiv.appendChild(ctaBtn);
 
-  const rightDiv = document.createElement('div');
-  rightDiv.setAttribute('class', 'right');
-  const img = document.createElement('img');
-  img.setAttribute('src', 'https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d3da99cb1eba4bb6d1caf2a_welcome.jpg');
+      return homeTextDiv;
+    };
 
-  rightDiv.appendChild(img);
+    leftDivTag.appendChild(homeText());
 
-  homeSection.appendChild(leftDiv);
-  homeSection.appendChild(rightDiv);
+    return leftDivTag;
+  };
 
-  return homeSection;
+  const rightDiv = () => {
+    const rightDivTag = document.createElement('div');
+    rightDivTag.setAttribute('class', 'right');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', 'https://uploads-ssl.webflow.com/5d3819c2c65d403ab61fe6cd/5d3da99cb1eba4bb6d1caf2a_welcome.jpg');
+
+    rightDivTag.appendChild(img);
+
+    return rightDivTag;
+  };
+
+  const homeSection = () => {
+    const homeSectionTag = document.createElement('section');
+    homeSectionTag.setAttribute('class', 'home');
+
+    homeSectionTag.appendChild(leftDiv());
+    homeSectionTag.appendChild(rightDiv());
+
+    return homeSectionTag;
+  };
+
+  return homeSection();
 })();
 
 export default home;
