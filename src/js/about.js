1fa7a1edca49b134eae25dba9139e7d2073ba042
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-07 20:21:08 +0530

    Refactor about section to use modular code

--- src/js/about.js
+++ src/js/about.js
@@ -1,31 +1,37 @@
 const about = (() => {
-  const aboutSection = document.createElement('section');
-  aboutSection.setAttribute('class', 'about');
+  const aboutText = () => {    
+    const aboutHeading = document.createElement('h2');
+    aboutHeading.setAttribute('class', 'about__heading');
+    aboutHeading.innerText = 'Who are we?';
+    
+    const para = (text) => {
+      const paragraph = document.createElement('p');
+      paragraph.setAttribute('class', 'para');
+      paragraph.innerText = text;
+      
+      return paragraph;
+    };
 
-  const aboutText = document.createElement('div');
-  aboutText.setAttribute('class', 'about__text');
+    const aboutTextDiv = document.createElement('div');
+    aboutTextDiv.setAttribute('class', 'about__text');
+        
+    aboutTextDiv.appendChild(aboutHeading);
+    aboutTextDiv.appendChild(para('Pots Midye is the first stop of street flavors we have established under the name of Seyyarhane. In our workshop, we produce mussels that we receive live from the relevant ministry-approved farms in accordance with the Turkish Food Codex and ship them to our branches.'));
+    aboutTextDiv.appendChild(para('In addition to the traditional flavor, we bring a new breath to the street kitchen with our different recipes, always hot, fresh and pots of mussels cooked specially for you whenever you want.'));
 
-  const aboutHeading = document.createElement('h2');
-  aboutHeading.setAttribute('class', 'about__heading');
-  aboutHeading.innerText = 'Who are we?';
+    return aboutTextDiv;
+  };
 
-  aboutText.appendChild(aboutHeading);
+  const aboutSection = () => {
+    const aboutSectionTag = document.createElement('section');
+    aboutSectionTag.setAttribute('class', 'about');
 
-  const para1 = document.createElement('p');
-  para1.setAttribute('class', 'para');
-  para1.innerText = 'Pots Midye is the first stop of street flavors we have established under the name of Seyyarhane. In our workshop, we produce mussels that we receive live from the relevant ministry-approved farms in accordance with the Turkish Food Codex and ship them to our branches.';
+    aboutSectionTag.appendChild(aboutText());
 
-  aboutText.appendChild(para1);
+    return aboutSectionTag;
+  };
 
-  const para2 = document.createElement('p');
-  para2.setAttribute('class', 'para');
-  para2.innerText = 'In addition to the traditional flavor, we bring a new breath to the street kitchen with our different recipes, always hot, fresh and pots of mussels cooked specially for you whenever you want.';
-
-  aboutText.appendChild(para2);
-
-  aboutSection.appendChild(aboutText);
-
-  return aboutSection;
+  return aboutSection();
 })();
 
 export default about;
